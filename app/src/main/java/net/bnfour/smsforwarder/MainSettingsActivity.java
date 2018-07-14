34a2f6ecf29048217a31bc47811233af2bd355e0
Bn4 <bnfour@bnfour.net>
2018-07-14 13:41:59 +0700

    basic android project created

---
+++ app/src/main/java/net/bnfour/smsforwarder/MainSettingsActivity.java
@@ -0,0 +1,13 @@
+package net.bnfour.smsforwarder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainSettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+}
